174f5ae57f867052d11cdab965ccd32bf174c5d2
azizmakri <makrimohamedaziz99@gmail.com>
2023-12-27 14:04:34 +0100

    webcommit

---
+++ App.ApplicationCore/Entities/Beer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace App.ApplicationCore.Entities
+{
+    public class Beer
+    {
+        public int BeerId { get; set; }
+        public string Name { get; set; }
+        public float AlcoholContent { get; set; }
+        public float Price { get; set; }
+
+        // Foreign key for Brewery
+        public int BreweryFK { get; set; }
+
+        // Navigation property for Brewery
+        public virtual Brewery Brewery { get; set; }
+
+        // Navigation properties for related entities
+        public virtual ICollection<Stock> Stocks { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; }
+
+
+
+    }
+
+
+
+}
---
+++ App.ApplicationCore/Entities/Brewery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.ApplicationCore.Entities
+{
+    public class Brewery
+    {
+        public int BreweryId { get; set; }
+
+        public string Name { get; set; }
+        // Navigation property for related Beers
+
+        public virtual ICollection<Beer> Beers { get; set; }
+
+
+    }
+}
